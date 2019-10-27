1bec674c49a81e9ea816b0d9ddc70be432725a56
khadai <mykhailo.khadai@gmail.com>
2019-10-27 14:16:53 +0200

    added connection change receiver

--- app/src/main/java/com/example/fisrtapplication/activities/DataListActivity.java
+++ app/src/main/java/com/example/fisrtapplication/activities/DataListActivity.java
@@ -1,16 +1,20 @@
 package com.example.fisrtapplication.activities;
 
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import com.example.fisrtapplication.R;
 import com.example.fisrtapplication.adapters.VendingsAdapter;
 import com.example.fisrtapplication.api.VendingApiClient;
 import com.example.fisrtapplication.entities.Vending;
 import com.example.fisrtapplication.utils.ApplicationEx;
+import com.example.fisrtapplication.utils.ConnectionChangeReceiver;
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.List;
@@ -30,6 +34,8 @@ public class DataListActivity extends AppCompatActivity {
     private VendingsAdapter vendingsAdapter;
     private SwipeRefreshLayout refreshLayout;
     private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
+    private View content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +43,9 @@ public class DataListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setupViews();
+        checkConnection();
         loadMovies();
-        swipeToRefresh();
+        setSwipeToRefresh();
     }
 
     @Override
@@ -47,30 +54,41 @@ public class DataListActivity extends AppCompatActivity {
         return true;
     }
 
+    private void checkConnection() {
+        IntentFilter filter = new IntentFilter("android.net.conn.CONNECTIVITY_CHANGE");
+        ConnectionChangeReceiver receiver = new ConnectionChangeReceiver(content);
+        this.registerReceiver(receiver, filter);
+    }
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        progressBar.setVisibility(View.VISIBLE);
         mAuth.signOut();
+        progressBar.setVisibility(View.INVISIBLE);
         Intent intent = new Intent(this, SignInActivity.class);
         startActivity(intent);
         return true;
     }
 
-    private void swipeToRefresh() {
+    private void setSwipeToRefresh() {
         refreshLayout.setOnRefreshListener(() -> {
             loadMovies();
-            new Handler().postDelayed(() -> refreshLayout.setRefreshing(false), 4000);
+            new Handler().postDelayed(() -> refreshLayout.setRefreshing(false), 0);
         });
     }
 
     private void setupViews() {
         recyclerView = findViewById(R.id.recycler_view);
         refreshLayout = findViewById(R.id.swipe_refresher);
+        progressBar = findViewById(R.id.progress_bar_main);
+        content = findViewById(R.id.content_view);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         mAuth = getApplicationEx().getAuth();
     }
 
     private void loadMovies() {
+        progressBar.setVisibility(View.VISIBLE);
         final VendingApiClient apiService = getApplicationEx().getVendingApiClient();
         final Call<List<Vending>> call = apiService.getVendings();
 
@@ -80,10 +98,12 @@ public class DataListActivity extends AppCompatActivity {
                                    final Response<List<Vending>> response) {
                 vendingsAdapter = new VendingsAdapter(response.body());
                 recyclerView.setAdapter(vendingsAdapter);
+                progressBar.setVisibility(View.INVISIBLE);
             }
 
             @Override
             public void onFailure(Call<List<Vending>> call, Throwable t) {
+                progressBar.setVisibility(View.INVISIBLE);
             }
         });
 
